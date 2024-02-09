61644d35984c3a18e959905209c10397db9acd82
Fatima Anthony <fati_jas@yahoo.com>
2024-02-09 11:04:33 -0800

    first commit

---
+++ src/main/java/fmacademy/App.java
@@ -0,0 +1,13 @@
+package fmacademy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
